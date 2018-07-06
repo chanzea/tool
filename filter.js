e228813c06da561763b2bd2bba4db70c932b4129
chanzea <876890520@qq.com>
2018-07-06 12:11:12 +0800

    '去重'

--- filter.js
+++ filter.js
@@ -1,2 +1,7 @@
 // 数组去重
 let filteredArray = _Array.filter((item, idx, arr) => { return arr.indexOf(item) === idx; })
+
+// 对象数组去除
+let result = targetArr.filter(item => {
+    return sourceArr.indexOf(item.id) < 0;
+})
